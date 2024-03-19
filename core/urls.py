7c84e1eb9854018936e6fc45a3a7ce99c657961c
Koosha Sangari <ko.sangari@gmail.com>
2024-03-19 13:27:18 +0100

    add core app.

---
+++ core/urls.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from app.api.v1.views.auth import register_user, login_user
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Move Lens API",
+        default_version="v1",
+        description="API documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path(
+        "api/v1/",
+        include(
+            [
+                path("movies/", include("app.api.v1.urls.movies")),
+                path(
+                    "auth/",
+                    include(("app.api.v1.urls.auth", "auth"), namespace="auth"),
+                ),
+            ]
+        ),
+    ),
+    path("admin/", admin.site.urls),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
